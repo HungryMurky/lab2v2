e4d4b47d62ac166e1920acc8269b8c5e07f5865c
Astakhau_Y <hungrymorlock1917@gmail.com>
2022-10-13 02:43:02 +0300

    apache common csv

--- src/lab/Main.java
+++ src/lab/Main.java
@@ -1,7 +1,38 @@
 package lab;
+import org.apache.commons.csv.*;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        /*List<Garage> GarageList = new ArrayList<Garage>();
+        List<Electricity> ElectricityList= new ArrayList<Electricity>();*/
+        String SAMPLE_CSV_FILE_PATH = "./SourceData.csv";
+        try (
+                Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+                CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT)
+        ) {
+            for (CSVRecord csvRecord : csvParser) {
+                // Accessing Values by Column Index
+                String name = csvRecord.get(0);
+                String email = csvRecord.get(1);
+                String phone = csvRecord.get(2);
+                String country = csvRecord.get(3);
 
+                System.out.println("Record No - " + csvRecord.getRecordNumber());
+                System.out.println("---------------");
+                System.out.println("Name : " + name);
+                System.out.println("Email : " + email);
+                System.out.println("Phone : " + phone);
+                System.out.println("Country : " + country);
+                System.out.println("---------------\n\n");
+            }
+
+            }
+        }
     }
-}
