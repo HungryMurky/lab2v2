05725957824c2007116815f67a5a5f2304c8c2ea
Astakhau_Y <hungrymorlock1917@gmail.com>
2022-10-12 17:30:47 +0300

    created variables for Garage class

--- src/lab/Garage.java
+++ src/lab/Garage.java
@@ -1,4 +1,26 @@
 package lab;
 
+import java.time.LocalDate;
+
 public class Garage {
+    int Number;
+    String Address;
+    String OwnerFullName;
+    LocalDate StartDate;
+    int CountBefore;
+    int CountNow;
+    int Price;
+    LocalDate PayDate;
+
+    public Garage(int number, String address, String ownerFullName, LocalDate startDate, int countBefore, int countNow, int price, LocalDate payDate) {
+        Number = number;
+        Address = address;
+        OwnerFullName = ownerFullName;
+        StartDate = startDate;
+        CountBefore = countBefore;
+        CountNow = countNow;
+        Price = price;
+        PayDate = payDate;
+    }
 }
+
