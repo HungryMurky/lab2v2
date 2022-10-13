9c67c99b70cd9b048343dc08bbbdaa2e602a4031
Astakhau_Y <hungrymorlock1917@gmail.com>
2022-10-13 19:30:17 +0300

    almost done, some few fixes required

--- src/lab/Main.java
+++ src/lab/Main.java
@@ -6,11 +6,14 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Main {
     private final static  String SAMPLE_CSV_FILE_PATH = "./SourceData.csv";
@@ -37,14 +40,40 @@ public class Main {
                  .forEach(e->System.out.println(e.getAddress()));
 
         System.out.println();
-        Scanner inp = new Scanner(System.in);
+        /*Scanner inp = new Scanner(System.in);
         System.out.println("Введите адрес кооператива");
         String address = inp.nextLine();
         System.out.println("Вывод списка владельцев гаражей заданного гаражного кооператива ("+address+") с указанием даты вступления в кооператив.");
          GarageList.stream()
                  .filter(e->Objects.equals(e.getAddress(), address))
-                 .forEach(e->System.out.println(e.getOwnerFullName()+" "+ e.getStartDate()));
+                 .forEach(e->System.out.println(e.getOwnerFullName()+" "+ e.getStartDate()));*/
 
+        LocalDate lt = LocalDate.now();
+
+        System.out.println("Определение количества платежей в каждом гаражном кооперативе для самых первых владельцев гаражей (т.е. те кто владеют гаражом дольше 10 лет).");
+         GarageList.parallelStream()
+                 .filter(e->e.StartDate.isBefore(lt.minusYears(10)))
+                 .forEach(e->System.out.println(e.getOwnerFullName()+" "+ e.getStartDate()
+                 +" Кол-во платежей: "+(Period.between(e.StartDate,lt).getMonths()+Period.between(e.StartDate,lt).getYears()*12)));
+
+        System.out.println("Определение минимального предыдущего показания счетчика для каждого гаражного кооператива");
+         ElectricityList.stream()
+                .sorted(Comparator.comparing(Electricity::getCountBefore))
+                .filter(distinctByKey(Electricity::getAddress))
+                .forEach(e->System.out.println(e.getCountBefore()+" "+e.getAddress()));
+
+        System.out.println("Определение средней стоимости за единицу для каждого владельца. " +
+                "Выводить ФИО владельца и среднюю стоимость за единицу в порядке убывания стоимости");
+         Map<String, List<Electricity>> EMap = ElectricityList.parallelStream()
+                 .sorted(Comparator.comparing(Electricity::getPrice))
+                 .collect(Collectors.groupingBy(Electricity::getOwnerFullName));
+         Map<String,Double> PMap = new HashMap<>();
+
+        EMap.forEach((k,v)->PMap.put(k,v.stream().mapToDouble(e->e.Price).average().getAsDouble()) );
+        List<Map.Entry<String, Double>> plist = new ArrayList<>(PMap.entrySet());
+        plist.sort(Map.Entry.comparingByValue());
+        Collections.reverse(plist);
+        plist.forEach(System.out::println);
         }
 
     public static List<Garage> CreateGaragesCollection(String path) throws IOException {
