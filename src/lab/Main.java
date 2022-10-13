073f829de9d55cd5e1599f636ce4396722646c99
Astakhau_Y <hungrymorlock1917@gmail.com>
2022-10-13 20:19:42 +0300

    final

--- src/lab/Main.java
+++ src/lab/Main.java
@@ -39,27 +39,35 @@ public class Main {
                  .forEach(e->System.out.println(e.getAddress()));
 
         System.out.println();
-        /*Scanner inp = new Scanner(System.in);
+        Scanner inp = new Scanner(System.in);
         System.out.println("Введите адрес кооператива");
         String address = inp.nextLine();
         System.out.println("Вывод списка владельцев гаражей заданного гаражного кооператива ("+address+") с указанием даты вступления в кооператив.");
          GarageList.stream()
                  .filter(e->Objects.equals(e.getAddress(), address))
-                 .forEach(e->System.out.println(e.getOwnerFullName()+" "+ e.getStartDate()));*/
+                 .forEach(e->System.out.println(e.getOwnerFullName()+" "+ e.getStartDate()));
 
+
+        System.out.println();
         LocalDate lt = LocalDate.now();
+        System.out.println("Самый дорогой гараж");
+        (ElectricityList.stream()
+                 .reduce((a,b)-> (a.getPrice()* (a.getCountNow()-a.getCountBefore()))>(b.getPrice()*(b.getCountNow()-b.getCountBefore())) ? a:b)).ifPresent(e->System.out.println("Номер: "+ e.getNumber()+" Сумма за прошлый месяц: "+e.getPrice()* (e.getCountNow()-e.getCountBefore())));
 
+        System.out.println();
         System.out.println("Определение количества платежей в каждом гаражном кооперативе для самых первых владельцев гаражей (т.е. те кто владеют гаражом дольше 10 лет).");
          GarageList.parallelStream()
                  .filter(e->e.StartDate.isBefore(lt.minusYears(10)))
                  .forEach(e->System.out.println(e.getOwnerFullName()+" "+ e.getStartDate()
                  +" Кол-во платежей: "+(Period.between(e.StartDate,lt).getMonths()+Period.between(e.StartDate,lt).getYears()*12)));
+        System.out.println();
 
         System.out.println("Определение минимального предыдущего показания счетчика для каждого гаражного кооператива");
          ElectricityList.stream()
                 .sorted(Comparator.comparing(Electricity::getCountBefore))
                 .filter(distinctByKey(Electricity::getAddress))
                 .forEach(e->System.out.println(e.getCountBefore()+" "+e.getAddress()));
+        System.out.println();
 
         System.out.println("Определение средней стоимости за единицу для каждого владельца. " +
                 "Выводить ФИО владельца и среднюю стоимость за единицу в порядке убывания стоимости");
@@ -67,12 +75,12 @@ public class Main {
                  .sorted(Comparator.comparing(Electricity::getPrice))
                  .collect(Collectors.groupingBy(Electricity::getOwnerFullName));
          Map<String,Double> PMap = new HashMap<>();
-
         EMap.forEach((k,v)->PMap.put(k,v.stream().mapToDouble(e->e.Price).average().getAsDouble()) );
         List<Map.Entry<String, Double>> plist = new ArrayList<>(PMap.entrySet());
         plist.sort(Map.Entry.comparingByValue());
         Collections.reverse(plist);
         plist.forEach(System.out::println);
+
         }
 
     public static List<Garage> CreateGaragesCollection(String path) throws IOException {
